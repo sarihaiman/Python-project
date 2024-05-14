3917bad58476516837d81ad8138fca1ce716f86c
sarihaiman <sari9741497@gmail.com>
2024-05-15 00:50:39 +0300

    Continue with the visualization

--- app/Services/Visualization.py
+++ app/Services/Visualization.py
@@ -8,15 +8,12 @@ async def plot():
     # response = requests.get('http://127.0.0.1:8000/budget?userid=0')
     # print(response)
     # if response.status_code == 200:
-    #     data = response.json()  # המרת התגובה ל JSON
-        # יצירת ויזואליזציה מתאימה (לדוגמה, גרף)
-        # הגדרת כותרת וכווץ
+    #     data = response.json()
         # plt.plot(data['revenues'], data['expenses'], marker='o', linestyle='-')
         plt.plot(45, 23, marker='o', linestyle='-')
         plt.title('Visualization of Data')
         plt.xlabel('X-axis')
         plt.ylabel('Y-axis')
-        # הצגת הוויזואליזציה
         plt.show()
     # # plt.savefig('static/plot.jpg')
     # # return render_template('plot.html')
